dfeea6e300997273641b16df0a97d6e40ea7a2fc
cuongtranba <bacuongtr@gmail.com>
2016-11-03 06:57:20 +0700

    add tags

--- myblog/views.py
+++ myblog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import Category 
+from .models import Category,Tag
+
 
 def index(request):
-    categories=Category.objects.all()
-    return render(request,"index.html",{'categories':categories})
+    categories = Category.objects.all()
+    tags=Tag.objects.all()
+    return render(request, "index.html", {'categories': categories,'tags':tags})
 
 
 
-    
