855f53a1202dc820c7382bd1ced8b65f8c943d6f
cuongtranba <bacuongtr@gmail.com>
2016-11-03 06:46:44 +0700

    add category

--- myblog/views.py
+++ myblog/views.py
@@ -1,17 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-import models
-# Create your views here.
-
+from .models import Category 
 
 def index(request):
-    template = loader.get_template("index.html")
-    return HttpResponse(template.render())
-
-def Category(request):
     categories=Category.objects.all()
-    return render(request,"category.html",{'categories':categories})
+    return render(request,"index.html",{'categories':categories})
+
 
 
     
