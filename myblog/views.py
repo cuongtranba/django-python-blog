1a089fe2a2e55cba04e0b5b4c3e02be950f2a43d
cuongtranba <bacuongtr@gmail.com>
2016-11-02 08:25:14 +0700

    add static files

--- myblog/views.py
+++ myblog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 def index(request):
-    response = HttpResponse()
-    response.write("<h1>Welcome</h1>")
-    response.write("This is the polls app")
-    return response
+    template=loader.get_template("index.html")
+    return HttpResponse(template.render())
+
+
