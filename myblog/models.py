12d8beaefb8610ea35ca54f67a88d58cb033b458
cuongtranba <bacuongtr@gmail.com>
2016-11-02 15:42:13 +0700

    no message

--- myblog/models.py
+++ myblog/models.py
@@ -12,17 +12,26 @@ class BaseModel(models.Model):
 
 
 class User(BaseModel):
-    name=models.CharField(max_length=20)
-    picture_url=models.CharField(max_length=200)
+    name = models.CharField(max_length=20)
+
+    picture_url = models.CharField(max_length=200)
 
 
 class Tag(BaseModel):
     tag_name = models.CharField(max_length=20)
 
+
+class Category(BaseModel):
+    category_name = models.CharField(max_length=200)
+
+
 class Post(BaseModel):
     title = models.CharField(max_length=200)
-    content=models.TextField()
-    pub_date = models.DateTimeField()
-    user=models.ForeignKey(User)
-    tag=models.ForeignKey(Tag)
+    content = models.TextField()
 
+    pub_date = models.DateTimeField()
+    user = models.ForeignKey(User,null=True)
+
+    tag = models.ForeignKey(Tag,null=True)
+
+    category = models.ForeignKey(Category,null=True)
--- myblog/views.py
+++ myblog/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+import models
 # Create your views here.
+
+
 def index(request):
-    template=loader.get_template("index.html")
+    template = loader.get_template("index.html")
     return HttpResponse(template.render())
 
+def Category(request):
+    categories=Category.objects.all()
+    return render(request,"category.html",{'categories':categories})
 
+
+    
